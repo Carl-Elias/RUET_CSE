37adc3f584c4f2a6e46d5cb0f53ceb2136533183
tonmoyislam250 <su6087031@gmail.com>
2022-10-26 13:56:44 +0600

    Hello
    Signed-off-by: tonmoyislam250 <su6087031@gmail.com>

--- push.py
+++ push.py
@@ -1,13 +1,6 @@
 import os
-f = open("../commit.txt", "w")
 str = input("Input your Commit message:\n")
-f.write(str)
-f.close
-f = open("../commit.txt", "a")
-str2 = f"\nAuthor : Tonmoy Islam \nEmail: tonmoyvyper@gmail.com"
-f.write(str2)
-f.close
 os.system("python3 ./rmv.py")
 os.system("git add -v .")
-os.system("git commit -F ../commit.txt")
+os.system("git commit --signoff -m"+str)
 os.system("git push")
