32739d9f35a93a208f1511b7d9b2dcbec24eae85
tonmoyislam250 <su6087031@gmail.com>
2022-10-26 15:48:18 +0600

    hello my Author: Tonmoy Islam Email: tonmoyvyper@gmail.com

--- push.py
+++ push.py
@@ -1,6 +1,11 @@
 import os
 str = input("Input your Commit message:\n")
+str2=str+"\nAuthor: Tonmoy Islam\nEmail: tonmoyvyper@gmail.com"
+f=open("c.txt","w")
+f.write(str2)
+f.close
+# str3="git commit -s -m"+"$(echo $str2)"
 os.system("python3 ./rmv.py")
 os.system("git add -v .")
-os.system("git commit --signoff -m"+str)
+os.system("git commit --file ./c.txt")
 os.system("git push")
