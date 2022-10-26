b70bd081b2af83c0f44118f2828665c263a9623f
tonmoyislam250 <su6087031@gmail.com>
2022-10-26 13:28:19 +0600

    next lab.. Author: Tonmoy Islam Email: tonmoyvyper@gmail.com

---
+++ 1204/module3/q4/inheritence.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+private:
+    int x;
+
+protected:
+    int y;
+
+public:
+    int z;
+};
+class B : public A
+{
+public:
+    int getx() { return x; }
+    int gety() { return y; }
+}
